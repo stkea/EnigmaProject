87b512463b2bfc7a205f4e2a0df6479a1c700f8d
Stefan <stef240p@stud.kea.dk>
2022-03-01 09:35:59 +0100

    added a better solution

--- EnigmaProtA/src/com/company/Main.java
+++ EnigmaProtA/src/com/company/Main.java
@@ -2,33 +2,17 @@ package com.company;
 
 public class Main {
 
-    char[] charArray = {
-            ' ',
-            'A', 'B', 'C',
-            'D', 'E', 'F',
-            'G', 'H', 'I',
-            'J', 'K', 'L',
-            'M', 'N', 'O',
-            'P', 'Q', 'R',
-            'S', 'T', 'U',
-            'V', 'W', 'X',
-            'Y', 'Z', 'Æ',
-            'Ø', 'Å'};
 
+    final String ALPHABET = " ABCDEFGHIJKLMNOPQRSTUVWXYZÆØÅ";
 
     public int letterToNumber(char charr) {
-        int index = 0;
-        for (int i = 0; i < charArray.length; i++) {
-            if (charArray[i] == charr) {
-                index = i;
-            }
-        }
-        return index;
+        return ALPHABET.indexOf(charr);
     }
 
-    public char numberToLetter(int number) {
-        return charArray[number];
+    public char numberToLetter(int index) {
+        return ALPHABET.charAt(index);
     }
+
 
     public static void main(String[] args) {
         Main main = new Main();
